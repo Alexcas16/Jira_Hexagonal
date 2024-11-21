b833a5591492450d5e046515ef683c507f1a7097
Alexcas16 <alejandrocastillollorens@gmail.com>
2024-11-21 08:53:23 +0100

    conexion con BD H2

--- src/main/java/com/jiraHex/tarea/infraestructura/database/h2/respositorio/TareaJpaRespositorio.java
+++ src/main/java/com/jiraHex/tarea/infraestructura/database/h2/respositorio/TareaJpaRespositorio.java
@@ -4,11 +4,15 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.jiraHex.tarea.infraestructura.database.h2.entidades.TareaEntity;
 
 @Repository
 public interface TareaJpaRespositorio extends JpaRepository<TareaEntity, UUID>{
-	Optional<TareaEntity> obtenerTareaPorId(Long tareaId);
+	
+	 @Query("SELECT t FROM TareaEntity t WHERE t.tareadId = :id")
+	 Optional<TareaEntity> obtenerTareaPorId(@Param("id") Long id);
 }

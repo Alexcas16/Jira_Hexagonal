9415600e546bc46c45e9c291931d31ef1c9eb03a
Alexcas16 <alejandrocastillollorens@gmail.com>
2024-11-22 14:02:34 +0100

    arreglar error en mongo repository

--- src/main/java/com/jiraHex/tarea/infraestructura/database/mongobd/repositorio/TareaMongoRepositorio.java
+++ src/main/java/com/jiraHex/tarea/infraestructura/database/mongobd/repositorio/TareaMongoRepositorio.java
@@ -5,10 +5,9 @@ import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
-import com.jiraHex.tarea.dominio.modelos.Tarea;
 import com.jiraHex.tarea.infraestructura.database.mongobd.entidades.MongoTareaEntity;
 
-public interface TareaMongoRepositorio extends MongoRepository<Tarea, String> {
+public interface TareaMongoRepositorio extends MongoRepository<MongoTareaEntity, String> {
 
 	@Query("{ 'tarea_id' : ?0 }")
 	Optional<MongoTareaEntity> obtenerTareaPorId(Long id);
